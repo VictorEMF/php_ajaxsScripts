926d9704578c57c76b75b5e17b08f19e0d23a387
Victor Emanuel <victoremanuel.mff@gmail.com>
2021-01-18 15:10:19 -0300

    Paginas de funcionario e Cliente

---
+++ pages/pageCli.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Pagina do Cliente</title>
+  </head>
+  <body>
+    <a href="#">Meus Pedidos</a>
+    <a href="#">Minhas Informações</a>
+  </body>
+</html>
---
+++ pages/pageFun.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Pagina do Funcionario</title>
+  </head>
+  <body>
+    <a href="#">Ver Informações dos Clientes</a>
+    <a href="#">Adicionar Clientes</a>
+    <a href="#">Editar Pedido dos Clientes</a>
+    <a href="#">Editar Cliente</a>
+  </body>
+</html>
