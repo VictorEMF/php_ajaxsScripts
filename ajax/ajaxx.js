ade1fafbe6c4e8e9d7cae9ce45cc11322150e60a
Victor Emanuel <victoremanuel.mff@gmail.com>
2021-01-25 12:42:12 -0300

    passa dados pro php

--- ajax/ajaxx.js
+++ ajax/ajaxx.js
@@ -1,20 +1,27 @@
-function buscar(opçoes){
-  let page = '../backend/editarStatus.php';
-  $.ajax({
+$(document).ready(function() {
 
-    type: 'POST',
-    dataType: 'html',
-    url: page,
-    beforeSend: function(){
-      $("#resposta").html("Carregando");
-    },
-    data: {opçoes: opçoes},
-    success: function(msg){
-      $("#resposta").html(msg)
-    }
+  $("[name=submit]").click(function() {
+
+    var cpf = $("[name=cpf]").val();
+    var id = $("[name=id]").val();
+    var status = $("[name=opçoes]").val();
+    $.ajax({
+      type: "POST",
+      url: "../backend/editarStatus.php",
+      data: {
+                  cpf: cpf,
+                  id: id,
+                  status: status
+      },
+      cache: false,
+      success: function(msg){
+                  $("[name=resposta]").html(msg)
+      },
+      error: function(xhr, status, error) {
+        console.error(xhr);
+      }
+    });
+    
   });
-}
 
-$("[name='editar']").click(function(){
-  buscar($("[name='opçoes']").val())
 });
