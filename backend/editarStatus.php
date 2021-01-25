62a398d6a2685daac147bc3befa0511fa185114d
Victor Emanuel <victoremanuel.mff@gmail.com>
2021-01-25 12:43:11 -0300

    faz edidção do status do pedido

--- backend/editarStatus.php
+++ backend/editarStatus.php
@@ -1,15 +1,19 @@
 <?php
+session_start();
 include('conexao.php');
 
+$data['cpf'] = $_POST['cpf'];
+$data['id'] = $_POST['id'];
+$data['status'] = $_POST['status']; 
+$info = json_encode($data);
+$dados =  json_decode($info, true);
 
+if($dados['cpf'] != "" and $dados['id']) {
+    $sql = "UPDATE pedidos SET status = '{$dados['status']}' WHERE cpf_cliente = '{$dados['cpf']}' AND id = {$dados['id']}";
+    $result = mysqli_query($conn, $sql);
+    echo "Atualizado amigo";
+}else{
+    echo "Falta dados!";
+}
 
-$cpf = $_POST['cpf_cliente'];
-$status = $_POST['opçoes'];
-
-$sql = "UPDATE pedidos SET status = '$status' WHERE cpf_cliente = '$cpf'";
-
-
-$conn-close();
- ?>
-
-<strong>Ediçao realizada</strong>
+?>
